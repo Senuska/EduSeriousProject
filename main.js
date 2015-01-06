789793303548463ff2b469b4a31bab7fac1b79a8
Senuska <senuskaaf@gmail.com>
2015-01-06 09:22:19 -0500

    Initial project structure.
    Adds in the initial file structure and all of the enchant.js scripts.

---
+++ main.js
@@ -0,0 +1,43 @@
+enchant();
+window.onload = function(){
+	
+	// Creates game variable and preloads art assets for every scene
+    var game = new Core(800, 600);
+	
+	// Creates the different scenes used throughout the game
+    var menu_scene = new Scene();
+    var play_scene = new Scene();
+    var how_to_play_scene = new Scene();
+	var credits_scene = new Scene();
+	var options_scene = new Scene();
+
+	var credit_group = new Group();
+    game.fps = 30;
+	
+    game.onload = function(){
+		
+		game.pushScene(menu_scene);
+		
+		// ----------------------
+		// Define the Menu Scene
+		// ----------------------
+		
+        // -------------------------
+		// Define How To Play Scene
+		// -------------------------
+		
+		// -------------------------
+		// Define the Credits Scene
+		// -------------------------
+	
+		// -------------------------
+		// Define the Options Scene
+		// -------------------------
+		
+		// ----------------------
+		// Define the Play Scene
+		// ----------------------
+
+		
+    game.start();
+};

3cf0e5f4d775fb21e6790602b648b96d9432f685
Justin Ortiz <jortiz3@spsu.edu>
2015-01-15 17:56:15 -0500

    And then there were Fractions!
    added fraction class, mixed number class, and a method to get the
    factors of an integer.

--- main.js
+++ main.js
@@ -45,3 +45,82 @@ window.onload = function(){
 	}	
     game.start();
 };
+
+var Fraction = Class.create({ 
+	
+	//attributes
+	var numerator;
+	var denominator;
+	
+    //constructor
+    initialize: function(numerator, denominator) {
+      this.numerator = numerator;
+	  this.denominator = denominator;
+    } 
+
+    //Simplifies the fraction
+    simplify: function() {
+	  var numeratorFactors = getFactorsFor(numerator);
+	  var denominatorFactors = getFactorsFor(denominator);
+	  var greatestCommonFactor = 0;
+	  
+	  for (var nfactors = 0; nfactors < numeratorFactors.length; nfactors++) {
+		  for (var dfactors = 0; dfactors < denominatorFactors.length; dfactors++) {
+			  if (numeratorFactors[nfactors] == denominatorFactors[dfactors]) {
+				  if (numeratorFactors[nfactors] > greatestCommonFactor) {
+					  greatestCommonFactor = numeratorFactors[nfactors];
+				  }  
+			  }
+		  }
+	  }
+	  
+	  if (greatestCommonFactor <= 1)
+		  return;
+	  
+	  numerator /= greatestCommonFactor;
+	  denominator /= greatestCommonFactor;
+  }
+});
+
+var MixedNumber = Class.create(Fraction, {
+	
+	//attributes
+	var wholeNumber;
+	
+	//constructors
+	initialize: function(wholeNumber, numerator, denominator) {
+		this.wholeNumber = wholeNumber;
+		this.numerator = numerator;
+		this.denominator = denominator;
+	}
+	initialize: function(wholeNumber, fraction) {
+		this.wholeNumber = wholeNumber;
+		this.numerator = fraction.numerator;
+		this.denominator = fraction.denominator;
+	}
+	
+	//simplifies the value of the mixed number
+	simplify: function() {
+		//gets the simplified whole number and associated fraction
+		while (this.numerator >= this.denominator){
+			this.numerator -= this.denominator;
+			wholeNumber++;
+		}
+		if (numerator == 0)
+			return;
+		//now that we have the whole number, we simplify the fraction using the superclass's simplify method
+		Fraction.call (simplify);
+	}
+});
+
+//returns the factors for a specified integer in the form of an array
+getFactorsFor: function(integer) {
+	var factors = new Array();
+	var counter = 0;
+	while (counter <= integer) {
+		if (integer % counter == 0)
+			factors.push(counter);
+		counter++;
+	}
+	return factors;
+}

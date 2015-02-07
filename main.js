7dc917a309f65208338fde8821c6b3fdf81f2994
Senuska <senuskaaf@gmail.com>
2015-02-07 11:18:41 -0500

    Initial Supply object definition
    Initial SupplyBag object definition
    
    TO-DO:
    	Implement the Generate method for SupplyBag
    	Implement to CalculateSize method for SupplyBag

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.onload = function(){
 	
 	// Creates game variable and preloads art assets for every scene
     var game = new Core(800, 600);
-	game.preload('assets/blue_background.gif', 'assets/green_background.gif', 'assets/orange_background.gif', 'assets/logo0.gif', 'assets/inventory_slot.gif', 'assets/inventory_background.gif');
+	game.preload('assets/blue_background.gif', 'assets/green_background.gif', 'assets/orange_background.gif', 'assets/logo0.gif', 'assets/inventory_slot.gif', 'assets/inventory_background.gif', 'assets/item_block01.gif', 'assets/item_block02.gif');
 	// Creates the different scenes used throughout the game
 	var main_scene = new Scene();
     var menu_scene = new Scene();
@@ -11,12 +11,12 @@ window.onload = function(){
     var howToPlay_scene = new Scene();
 	var credits_scene = new Scene();
 	var options_scene = new Scene();
-	var play_label = MakeLabel("Play", '16pt tahoma', 'black', 50, 100);
 	var play_button = new Button("Play", "dark", 50, 150);
 	var credits_button = new Button("Credits", "dark", 50, 150);
 	var logo = new Sprite(775, 300);
 	var inventory_background = new Sprite(100, 200);
-	var inventory = new Group();
+	var expedition_bag = new Group();
+	var item_block = [new Sprite(32, 32), new Sprite(32, 64)];
 
     game.fps = 30;
 	
@@ -29,7 +29,25 @@ window.onload = function(){
 		logo.image = game.assets['assets/logo0.gif'];
 		logo.x = game.width/2 - logo.width/2;
 		inventory_background.image = game.assets['assets/inventory_background.gif'];
-		inventory.addChild(inventory_background);
+		expedition_bag.addChild(inventory_background);
+		item_block[0].image = game.assets['assets/item_block01.gif'];
+		item_block[1].image = game.assets['assets/item_block02.gif'];
+		
+		/* Create Class for supplies */
+		var Item = Class.create(Sprite, {
+			initialize: function(image, width, height) {
+				this.image = image;
+				this.width = width;
+				this.height = height;
+				var fraction_value;
+				var name;
+			}
+			
+			
+		});
+		
+		var item_thing = new Item(game.assets['assets/item_block01.gif'], 50, 50);
+		
 		
 		// ----------------------
 		// Define the Menu Scene
@@ -95,23 +113,20 @@ window.onload = function(){
 		// Define the Play Scene
 		// ----------------------
 		play_scene.addChild(background[2]);
+		
 		/* Inventory Grid */
-		for(var i = 0; i < 5; i++) {
+		/*for(var i = 0; i < 5; i++) {
 			for(var j = 0; j < 11; j++) {
 				var tempSprite = new Sprite(32, 32);
-				tempSprite.x = inventory.x + i*17;
-				tempSprite.y = inventory.y + j*17;
+				tempSprite.x = expedition_bag.x + i*17;
+				tempSprite.y = expedition_bag.y + j*17;
 				tempSprite.scaleX = 0.5;
 				tempSprite.scaleY = 0.5;
 				tempSprite.image = game.assets['assets/inventory_slot.gif'];
-				inventory.addChild(tempSprite);
+				expedition_bag.addChild(tempSprite);
 			}
-		}
-		inventory.x = 100;
-		inventory.y = 100;
-		DragAndDrop(inventory);
+		}*/
 		
-		play_scene.addChild(inventory);
 		
 		
 		/* Back Button */
@@ -122,6 +137,47 @@ window.onload = function(){
 	}	
     game.start();
 };
+// Definition for the Supply object
+function Supply(sprite, id, value) {
+	
+	// Graphical representation for this supply
+	this.sprite = sprite;
+	// The fraction value of this supply
+	this.value = value;
+	// The user-generated ID for the supply
+	this.id = id;
+	
+	function AddToSupplyBag(bag) {
+		// Add this to the array in the bag
+		bag.contents.push(this);
+	}
+};
+
+// Definition for the SupplyBag Object
+function SupplyBag(size) {
+	this.size = size;
+	this.contents = [];
+	
+	function Contains(supply) {
+		for(i = 0; i < this.contents.length; i++) {
+			if(contents[i].id == supply.id) {
+				// The specified supply has been found
+				return true;
+			}
+		}
+		
+		// If it doesn't find the supply in the bag
+		return false;
+	}
+	
+	function CalculateSize() {
+		// Sum all of the fraction values of the supplies into one fraction value
+	}
+	
+	function Generate() {
+		// Create a grid that will scale with an expedition’s count size
+	}
+}
 
 function MakeLabel(text, font, color, x, y) {
 	var tempLabel = new Label(text);
@@ -142,6 +198,12 @@ function MakeButton(text, font, theme, height, width, x, y, event) {
 	return tempButton;
 };
 
+function MakeSprite(image, width, height) {
+	var tempSprite = new Sprite(width, height);
+	tempSprite.image = image;
+	return tempSprite;
+};
+
 function DragAndDrop(entity) {
 	entity.addEventListener(Event.TOUCH_START, function (e) {
 		var deltaX = entity.x - e.x;
@@ -156,6 +218,14 @@ function DragAndDrop(entity) {
 		entity.x = e.x;
 		entity.y = e.y;
 	});
+}
+
+function SnapToInventoryGrid(inventory, entity) {
+	for(var i = 0; i < inventory.childNodes.length; i++) {
+		if(Math.abs(inventory.childNodes[i].x - entity.x) < 2) {
+			entity.x = inventory.childNodes[i].x;
+		}
+	}
 }
 
 function FitToScreen(entity, game) {
